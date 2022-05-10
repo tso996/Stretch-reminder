5cf8e19ca356a69a79edf9e76b3bae8f97293b1e
Joe <51638822+tso996@users.noreply.github.com>
2022-05-10 15:42:04 +0100

    added thread

--- main.py
+++ main.py
@@ -10,5 +10,30 @@
 # os.system('Tell application "System Events" to display dialog "'+body_str+'" with title "'+title_str+'"')
 
 import easygui
+import time;
+from threading import Thread
+
+
+start = time.time()
+status = False
+
+def threaded_function(arg):
+    for i in range(arg):
+        print("running")
+        easygui.msgbox("It's been a while. Get up and excercise for a bit!", title="Stretch reminder")
+       
+        time.sleep(1)
+
+
 
 easygui.msgbox("It's been a while. Get up and excercise for a bit!", title="Stretch reminder")
+start = time.time()
+
+if __name__ == "__main__":
+    thread = Thread(target = threaded_function, args = (10, ))
+    while True:
+     if time.time() - start > 5:
+        start = time.time()
+        thread.start()
+        thread.join()
+    print("thread finished...exiting")
