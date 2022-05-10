d0ca1f2a96396c3d1bb579e0475adfc73c034e4e
Joe <51638822+tso996@users.noreply.github.com>
2022-05-10 15:19:13 +0100

    pop up window

---
+++ main.py
@@ -0,0 +1,14 @@
+# make a pop up window
+# make the timer
+# import os
+
+# body_str="It's been a while. Get up and stretch a little!"
+
+# title_str="stretch reminder"
+
+
+# os.system('Tell application "System Events" to display dialog "'+body_str+'" with title "'+title_str+'"')
+
+import easygui
+
+easygui.msgbox("It's been a while. Get up and excercise for a bit!", title="Stretch reminder")
